1c102fe847af996bdafba7e1b3d80a87cd921140
gowtam-potluri <potluri.g@northeastern.edu>
2021-08-12 22:29:38 -0400

    Changes

--- auth.js
+++ auth.js
@@ -59,7 +59,8 @@ routes.post('/signin', async (req, res) => {
     signedIn: true, givenName, name, email,
   };
   const token = jwt.sign(credentials, JWT_SECRET);
-  res.cookie('jwt', token, { httpOnly: true, domain: process.env.COOKIE_DOMAIN });
+  console.log(process.env.COOKIE_DOMAIN,'DOMAIN');
+  res.cookie('jwt', token, { httpOnly: true });
   res.json(credentials);
 });
 routes.post('/signout', async (req, res) => {
